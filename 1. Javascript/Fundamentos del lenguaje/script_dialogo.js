aaaecdc5f9d8f3a56b5ab2fb0cd2f5da88a8b873
Marcos Legido <lainmlh@gmail.com>
2023-09-26 00:51:17 +0200

    clase 26-09-23

---
+++ 1. Javascript/Fundamentos del lenguaje/script_dialogo.js
@@ -0,0 +1,3 @@
+// se pregunta un dato al usuario
+let nombre = prompt('Dame tu nombre:', 'Pepe');
+alert("Te llamas " + nombre);
---
+++ 1. Javascript/Fundamentos del lenguaje/script_global.js
@@ -0,0 +1,4 @@
+// funcion que afecta al ámbito global
+function mostrar() {
+    variable_global = "hola";
+}
